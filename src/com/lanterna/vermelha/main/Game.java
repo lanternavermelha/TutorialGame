46cfa898a4261c89925b57b6d77244d954ada7ee
lanternavermelha <f_figueira@hotmail.com>
2020-03-29 02:22:12 +0100

    created game thread and game loop

--- src/com/lanterna/vermelha/main/Game.java
+++ src/com/lanterna/vermelha/main/Game.java
@@ -4,21 +4,65 @@ import java.awt.*;
 
 public class Game extends Canvas implements Runnable {
 
-    public synchronized void start() {
-
-    }
-
     public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    private Thread thread; //single thread game (not recommended, unless its a simple game)
+    private boolean running = false;
 
     public Game() {
         new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this); //instantiates a new thread of our Game(this) class
+        thread.start(); //starts the thread
+        running = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join(); //thread is stopped
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         new Game();
     }
 
+    // game loop
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) /ns;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
 
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void render() {
+    }
+
+    private void tick() {
     }
 }
